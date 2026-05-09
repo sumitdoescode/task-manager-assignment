04bd231bae8ad04e0c2bc1d70c9c663f3ca21ea5
sumitdoescode <sumit.does.code@gmail.com>
2026-05-09 23:59:29 +0530

    refactor: update proxy middleware to handle route-based authentication redirects and matchers

--- proxy.ts
+++ proxy.ts
@@ -1,19 +1,24 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { auth } from "./lib/auth";
-import { headers } from "next/headers";
 
 export async function proxy(request: NextRequest) {
+    const { pathname } = request.nextUrl;
     const session = await auth.api.getSession({
-        headers: await headers(),
+        headers: request.headers,
     });
 
-    if (!session?.user) {
+    if (pathname.startsWith("/dashboard") && !session?.user) {
         return NextResponse.redirect(new URL("/", request.url));
     }
+
+    if (pathname === "/" && session?.user) {
+        return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+
     return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/dashboard"],
+    matcher: ["/", "/dashboard/:path*"],
 };
